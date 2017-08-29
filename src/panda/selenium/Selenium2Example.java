5c68c681234bdea8bbe7d0969a0367966e67f92e
liempt <liempt@aureole-it.vn>
2017-08-29 16:55:32 +0700

    Read excel files/listFilm.xlsx

--- src/panda/selenium/Selenium2Example.java
+++ src/panda/selenium/Selenium2Example.java
@@ -7,7 +7,16 @@ import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.StringSelection;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -16,9 +25,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Selenium2Example {
 
-	public static void main(String[] args) throws AWTException, InterruptedException {
+	private static final String FILE_NAME = "files/listFilm.xlsx";
 
-		String suffixSearch = " poster film";
+	private static final String SUFFIX_SEARCH = " poster film";
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws AWTException, InterruptedException, IOException {
 
 		String[] arr = new String[] { "The Cabin in the Woods", "Nightfall (2012)", "Aloha (2015)", "Sick Nurses ",
 				"Blood Out (2011)", "Friends With Benefits (2011)", "Dream House (2011)" };
@@ -27,16 +39,37 @@ public class Selenium2Example {
 
 		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
 
-		WebDriver driver = new ChromeDriver();
+//		WebDriver driver = new ChromeDriver();
 
-		driver.get(
-				"https://www.google.com.vn/search?q=image&rlz=1C1CHZL_viVN749VN749&tbs=isz:lt,islt:vga&tbm=isch&source=lnt&sa=X&ved=0ahUKEwjijOvphfzVAhVMOI8KHeJrCH0QpwUIGQ&biw=1273&bih=659&dpr=1");
+		// Advance image search with size large 640x480
+//		driver.get(
+//				"https://www.google.com.vn/search?q=image&rlz=1C1CHZL_viVN749VN749&tbs=isz:lt,islt:vga&tbm=isch&source=lnt&sa=X&ved=0ahUKEwjijOvphfzVAhVMOI8KHeJrCH0QpwUIGQ&biw=1273&bih=659&dpr=1");
 
+		FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
+		Workbook workbook = new XSSFWorkbook(excelFile);
+		Sheet sheet = workbook.getSheetAt(0);
+
+		int colTitle = 6;
+		
+		for (int rowIndex = 0; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+			Row row = sheet.getRow(rowIndex);
+			if (row != null) {
+				Cell cell = row.getCell(colTitle);
+				if (cell != null) {
+					// Found column and there is value in the cell.
+					String nameFilm = cell.getStringCellValue() == null ? "fox baby" : cell.getStringCellValue();
+					
+					System.out.println(nameFilm);
+				}
+			}
+		}
+
+		/*
 		for (String str : arr) {
 
 			WebElement searchBox = driver.findElement(By.name("q"));
 			searchBox.clear();
-			searchBox.sendKeys(str + suffixSearch);
+			searchBox.sendKeys(str + SUFFIX_SEARCH);
 			searchBox.sendKeys(Keys.RETURN);
 
 			// Point coordinates = searchBox.getLocation();
@@ -105,7 +138,9 @@ public class Selenium2Example {
 			// driver.close();
 		}
 		
+
 		driver.quit();
+		*/
 	}
 
 }
