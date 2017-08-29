263db67a84ed4b236e9f683e3e548ee17507ead6
liempt <liempt@aureole-it.vn>
2017-08-29 17:02:52 +0700

    Call selenium

--- src/panda/selenium/Selenium2Example.java
+++ src/panda/selenium/Selenium2Example.java
@@ -39,11 +39,11 @@ public class Selenium2Example {
 
 		System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
 
-//		WebDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
 
-		// Advance image search with size large 640x480
-//		driver.get(
-//				"https://www.google.com.vn/search?q=image&rlz=1C1CHZL_viVN749VN749&tbs=isz:lt,islt:vga&tbm=isch&source=lnt&sa=X&ved=0ahUKEwjijOvphfzVAhVMOI8KHeJrCH0QpwUIGQ&biw=1273&bih=659&dpr=1");
+		//// Advance image search with size large 640x480
+		driver.get(
+				"https://www.google.com.vn/search?q=image&rlz=1C1CHZL_viVN749VN749&tbs=isz:lt,islt:vga&tbm=isch&source=lnt&sa=X&ved=0ahUKEwjijOvphfzVAhVMOI8KHeJrCH0QpwUIGQ&biw=1273&bih=659&dpr=1");
 
 		FileInputStream excelFile = new FileInputStream(new File(FILE_NAME));
 		Workbook workbook = new XSSFWorkbook(excelFile);
@@ -51,7 +51,7 @@ public class Selenium2Example {
 
 		int colTitle = 6;
 		
-		for (int rowIndex = 0; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
+		for (int rowIndex = 1; rowIndex <= sheet.getLastRowNum(); rowIndex++) {
 			Row row = sheet.getRow(rowIndex);
 			if (row != null) {
 				Cell cell = row.getCell(colTitle);
@@ -59,10 +59,82 @@ public class Selenium2Example {
 					// Found column and there is value in the cell.
 					String nameFilm = cell.getStringCellValue() == null ? "fox baby" : cell.getStringCellValue();
 					
+					
+					//// Call selenium
 					System.out.println(nameFilm);
+					
+					WebElement searchBox = driver.findElement(By.name("q"));
+					searchBox.clear();
+					searchBox.sendKeys(nameFilm + SUFFIX_SEARCH);
+					searchBox.sendKeys(Keys.RETURN);
+
+					// Point coordinates = searchBox.getLocation();
+					Robot robot = new Robot();
+					robot.mouseMove(75, 400);
+					robot.mousePress(InputEvent.BUTTON1_MASK);
+					Thread.sleep(800);
+
+					robot.mousePress(InputEvent.BUTTON1_MASK);
+					robot.mouseRelease(InputEvent.BUTTON1_MASK);
+					Thread.sleep(800);
+
+					robot.mousePress(InputEvent.BUTTON1_MASK);
+					robot.mouseRelease(InputEvent.BUTTON1_MASK);
+					Thread.sleep(800);
+
+					robot.mousePress(InputEvent.BUTTON1_MASK);
+					robot.mouseRelease(InputEvent.BUTTON1_MASK);
+					Thread.sleep(800);
+
+					robot.mousePress(InputEvent.BUTTON1_MASK);
+					robot.mouseRelease(InputEvent.BUTTON1_MASK);
+					Thread.sleep(500);
+
+					robot.mouseMove(200, 500);
+					Thread.sleep(800);
+
+					robot.mousePress(InputEvent.BUTTON3_DOWN_MASK);
+					robot.mouseRelease(InputEvent.BUTTON3_DOWN_MASK);
+					Thread.sleep(800);
+
+					// 8 lan - Select Save as...
+					robot.keyPress(KeyEvent.VK_DOWN);
+					Thread.sleep(200);
+					robot.keyPress(KeyEvent.VK_DOWN);
+					Thread.sleep(200);
+					robot.keyPress(KeyEvent.VK_DOWN);
+					Thread.sleep(200);
+					robot.keyPress(KeyEvent.VK_DOWN);
+					Thread.sleep(200);
+					robot.keyPress(KeyEvent.VK_DOWN);
+					Thread.sleep(200);
+					robot.keyPress(KeyEvent.VK_DOWN);
+					Thread.sleep(200);
+					robot.keyPress(KeyEvent.VK_DOWN);
+					Thread.sleep(200);
+					robot.keyRelease(KeyEvent.VK_DOWN);
+
+					robot.keyPress(KeyEvent.VK_ENTER);
+					Thread.sleep(200);
+
+					StringSelection selection = new StringSelection(nameFilm.replace(" ", "_"));
+					Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+					clipboard.setContents(selection, selection);
+
+					robot.keyPress(KeyEvent.VK_CONTROL);
+					robot.keyPress(KeyEvent.VK_V);
+					Thread.sleep(300);
+					robot.keyRelease(KeyEvent.VK_CONTROL);
+					robot.keyRelease(KeyEvent.VK_V);
+
+					Thread.sleep(800);
+					robot.keyPress(KeyEvent.VK_ENTER);
+					robot.keyRelease(KeyEvent.VK_ENTER);
 				}
 			}
 		}
+		
+		driver.quit();
 
 		/*
 		for (String str : arr) {
